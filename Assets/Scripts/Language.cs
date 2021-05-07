7011b86b9b554898d4605ff60e7c8cb09f6d84ce
mazohere <akira.harris@education.nsw.gov.au>
2021-05-07 14:58:52 +1000

    eep

---
+++ Assets/Scripts/Language.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class Language : MonoBehaviour
+{
+
+    public InputField GetGrossSalaryText;
+    public Dropdown GetSalaryPayPeriodText;
+    public Text NetIncomeText;
+    public Text TaxPaidText;
+    public Text MedicareLevyText;
+    public Dropdown GetLanguage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
